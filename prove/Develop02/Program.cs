fbe230e6b4b05d6cc0fc930c8a311eb90b32a04f
whansen340 <peteyhansen340@gmail.com>
2024-01-26 19:52:47 -0700

    1

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Time to Journal! please choose an option.");
+        writeline ("1. Make an entry")
+        writeline ("2. Display")
+        writeline ("3. Load")
+        writeline ("4. Save")
+        writeline ("5. Quit")
+
+        int selection1 = (1);
+        int selection2 = (2);
+
     }
+    
 }
