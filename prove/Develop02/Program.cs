e3c302a8d5d258487f43768b0345731beaea75b6
whansen340 <peteyhansen340@gmail.com>
2024-01-26 20:19:21 -0700

    1

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,15 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Time to Journal! please choose an option.");
-        writeline ("1. Make an entry")
-        writeline ("2. Display")
-        writeline ("3. Load")
-        writeline ("4. Save")
-        writeline ("5. Quit")
+        
 
-        int selection1 = (1);
-        int selection2 = (2);
+    }
+    string filename = "journal.txt";
 
+    string option1
+    {
+        Console.WriteLine("1. Entry");
     }
     
 }
