34b815446069823c92bbd6ca99138bc41223f9eb
whansen340 <peteyhansen340@gmail.com>
2024-01-26 20:37:33 -0700

    3

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,14 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Time to Journal! please choose an option.");
-        
+        option1();
+        option2();
+        option3();
+        option4();
+        option5();
+
 
     }
-    string filename = "journal.txt";
+    
 
-    string option1
+    static void option1()
     {
         Console.WriteLine("1. Entry");
+    }
+    static void option2()
+    {
+        Console.WriteLine("2. Display")
+    }
+    static void option3()
+    {
+        Console.WriteLine("3. Load")
+    }
+    static void option4()
+    {
+        Console.WriteLine("4. Save")
+    }
+    static void option5()
+    {
+        Console.WriteLine("5. Quite")
     }
     
 }
