1932dcdd4069cbd6a5e309a2abe9fdccd7a91bfb
whansen340 <peteyhansen340@gmail.com>
2024-02-29 17:20:57 -0700

    mind

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -3,7 +3,57 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop04 World!");
+    {   
+        string answer = "1";
+        int seconds = 0;
+
+        do {
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("Menu Options: ");
+            Console.WriteLine("   1. Start breathing activity");
+            Console.WriteLine("   2. Start reflecting activity");
+            Console.WriteLine("   3. Start listing activity");
+            Console.WriteLine("   4. Quit");
+            Console.Write("Select a choice from the menu: ");
+            answer = Console.ReadLine();
+            Console.Clear();
+
+            if (answer == "1"){
+                BreathingActivity activity = new BreathingActivity("This activity will help you relax by walking you though breathing in and out slowly. Clear your mind and focus on your breathing.",
+                "Well done!", "Breathing Activity");
+                activity.StartingScreen();
+                activity.Script();
+                seconds += activity.GetTimeInput();
+            }
+            else if (answer == "2"){
+                ReflectingActivity activity = new ReflectingActivity("This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.",
+                "Well done!", "ReflectingActivity");
+                activity.StartingScreen();
+                Console.Clear();
+                activity.Script();
+                seconds += activity.GetTimeInput();
+            }
+            else if (answer == "3") {
+                ListingActivity activity = new ListingActivity("This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.",
+                "Well done!", "ListingActivity");
+                activity.StartingScreen();
+                Console.Clear();
+                activity.Script();
+                seconds += activity.GetTimeInput();
+            }
+            else if (answer == "4"){
+                Activity activity = new Activity("none", "none", "none");
+                Console.WriteLine($" --- You have comepleted {seconds} seconds of mindfulness activities this session. ---");
+                activity.Thinking(10);
+            }
+
+        } while (answer != "4");
     }
 }
+
+
+
+
+
+
