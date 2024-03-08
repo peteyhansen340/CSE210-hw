4afae014d6a58356d4bb89e81533c93435800d17
whansen340 <peteyhansen340@gmail.com>
2024-03-07 19:19:06 -0700

    L5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,30 @@
-using System;
+﻿using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("blue", 5);
+
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("green", 3, 9);
+
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("yellow", 2);
+
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea(); 
+            Console.WriteLine($"The color is: {color}");
+            Console.WriteLine($"The area is: {area}");
+        }
+        
     }
 }
